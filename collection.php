87017da5e8801dd7803f0568fd450607e5cd449d
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-03 22:02:52 +0530

    Update collection.php

--- collection.php
+++ collection.php
@@ -3,7 +3,7 @@ session_start();
 $show = $_POST['display'];
 
 if($show){
-$username=$_SESSION['username'];
+/*$username=$_SESSION['username'];
 $host='sql12.freesqldatabase.com';
 $dbuser='sql12345161';
 $dbpassword='3dqYuAVkkt';
@@ -12,7 +12,7 @@ $con=mysqli_connect($host,$dbuser,$dbpassword,$dbname);
 
 if(!$con){
 die('not connected');}
-mysqli_close($con);
+mysqli_close($con);*/
 echo "<script>  
   location.replace('https://indite.herokuapp.com/display.php');   
   </script>";
