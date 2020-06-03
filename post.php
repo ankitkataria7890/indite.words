982b27aa9039621d31a830472d348b6071e780ad
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-03 15:52:26 +0530

    Update post.php

--- post.php
+++ post.php
@@ -4,7 +4,12 @@ $post=$_POST['user_post'];
 if($post){
 $username=$_SESSION['username'];
 $date=$_SESSION['savedate'];
-$con= mysqli_connect('localhost','root','','dbsignup');
+$username=$_SESSION['username'];
+$host='sql12.freesqldatabase.com';
+$dbuser='sql12345161';
+$dbpassword='3dqYuAVkkt';
+$dbname='sql12345161';
+$con=mysqli_connect($host,$dbuser,$dbpassword,$dbname);
 if(!$con){
 die('not connected');}
 $q="INSERT INTO post". "(username,postdate)". "VALUES('$username','$date')";
