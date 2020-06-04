07ab7f985bf13bd537496d3b10b176d32ed584d6
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-05 01:16:27 +0530

    Update post.php

--- post.php
+++ post.php
@@ -16,12 +16,12 @@ die('not connected');}
 $q="INSERT INTO post". "(username,postdate)". "VALUES('$username','$date')";
 $i=mysqli_query($con,$q);
 $s="select from content where username='$username' && date='$date'";
-$sc=mysqli_query($con,$sc);
+$sc=mysqli_query($con,$s);
 $sf=mysqli_fetch_array($sc);
 $textarea=$sf['textarea'];
   $fontstyle=$sf['fontstyle'];
   $fontcolor=$sf['fontcolor'];
-  $text_size=$sf['textarea'];
+  $text_size=$sf['text_size'];
   $bgcolor=$sf['bgcolor'];
   
 $si="INSERT INTO postdata". "(username,textarea,date,fontstyle,fontcolor,text_size,bgcolor)". "VALUES('$username','$textarea','$date','$fontstyle','$fontcolor','$text_size','$bgcolor')";
