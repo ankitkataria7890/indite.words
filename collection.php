c71ec4ba4e067a0c43454c8afa342164ccfd6d22
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-16 16:45:28 +0530

    Update collection.php

--- collection.php
+++ collection.php
@@ -4,11 +4,7 @@ $show = $_POST['display'];
 header("Refresh:  300;url='REDIRECTION URI'");
 if($show){
 $username=$_SESSION['username'];
-$host='sql12.freesqldatabase.com';
-$dbuser='sql12345161';
-$dbpassword='3dqYuAVkkt';
-$dbname='sql12345161';
-$con=mysqli_connect($host,$dbuser,$dbpassword,$dbname);
+include 'database.php';
 
 if(!$con){
 die('not connected');}
