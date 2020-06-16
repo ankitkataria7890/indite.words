5a931119d8a795164819642e08b5f63038f8ebad
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-16 16:46:35 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,12 +6,7 @@ $post=$_POST['user_delete'];
 if($post){
 $username=$_SESSION['username'];
 $date=$_SESSION['savedate'];
-$username=$_SESSION['username'];
-$host='sql12.freesqldatabase.com';
-$dbuser='sql12345161';
-$dbpassword='3dqYuAVkkt';
-$dbname='sql12345161';
-$con=mysqli_connect($host,$dbuser,$dbpassword,$dbname);
+include 'database.php';
 if(!$con){
 die('not connected');}
 $q="DELETE FROM content where username='$username'&& date='$date'";
