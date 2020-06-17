312c59432dce17aabb33b92bb656eff87ff3c850
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-18 00:52:54 +0530

    Update user_suggestion.php

--- user_suggestion.php
+++ user_suggestion.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
 header("Refresh:  300;url='REDIRECTION URI'");
-$username=$_POST['username'];
-$name=$_POST['name'];
+$username=$_SESSION['username'];
+$fname=$_POST['firstname'];
+$lname=$_POST['firstname'];
 $email=$_POST['email'];
-$suggestion=$_POST['suggestion'];
+$suggestion=$_POST['suggest'];
 include 'database.php';
-$i="insert into suggestion (username,name,email,suggestion) values ('$username','$name','$email','$suggestion')";
+$i="insert into suggestion (username,fname,lname,email,suggestion) values ('$username','$fname','$lname','$email','$suggestion')";
 $ic=mysqli_query($con,$i);
+mysqli_close($con);
 if(!$ic)
 {
 echo " 
